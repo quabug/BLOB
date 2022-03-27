fec9a6f06a6a70c7a3bdfb6e08a35cffa14ae294
quabug <quabug@gmail.com>
2022-03-27 23:10:34 +0800

    benchmark different methods of binary writer.

---
+++ Assets/Benchmark/BinaryWrite.cs
@@ -0,0 +1,70 @@
+using System.IO;
+using NUnit.Framework;
+using Unity.PerformanceTesting;
+
+namespace Benchmark
+{
+    public class BinaryWriteMethods
+    {
+        [Test, Performance]
+        public unsafe void Write1MB([Values(8, 128, 512, 1024, 1024*256)] int chunkSize)
+        {
+            var totalSize = 1024 * 1024;
+            var iterCount = totalSize / chunkSize;
+            var chunk = new byte[chunkSize];
+            var stream = new MemoryStream();
+            const int measurementCount = 3;
+
+            Measure.Method(() =>
+                {
+                    for (var index = 0; index < chunkSize; index++) stream.WriteByte(chunk[index]);
+                })
+                .SampleGroup("WriteByte")
+                .SetUp(() => stream = new MemoryStream(totalSize))
+                .MeasurementCount(measurementCount)
+                .IterationsPerMeasurement(iterCount)
+                .Run()
+            ;
+
+            Measure.Method(() =>
+                {
+                    fixed (byte* ptr = &chunk[0])
+                    {
+                        using var unmanagedStream = new UnmanagedMemoryStream(ptr, chunkSize);
+                        unmanagedStream.CopyTo(stream);
+                    }
+                })
+                .SampleGroup("UnmanagedStream.CopyTo")
+                .SetUp(() => stream = new MemoryStream(totalSize))
+                .MeasurementCount(measurementCount)
+                .IterationsPerMeasurement(iterCount)
+                .Run()
+            ;
+
+            Measure.Method(() =>
+                {
+                    using var writer = new BinaryWriter(stream);
+                    writer.Write(chunk);
+                })
+                .SampleGroup("BinaryWrite")
+                .SetUp(() => stream = new MemoryStream(totalSize))
+                .MeasurementCount(measurementCount)
+                .IterationsPerMeasurement(iterCount)
+                .Run()
+            ;
+
+#if UNITY_2021_2_OR_NEWER
+            Measure.Method(() =>
+                {
+                    stream.Write(chunk);
+                })
+                .SampleGroup("Stream.Write")
+                .SetUp(() => stream = new MemoryStream(totalSize))
+                .MeasurementCount(measurementCount)
+                .IterationsPerMeasurement(iterCount)
+                .Run()
+            ;
+#endif
+        }
+    }
+}
