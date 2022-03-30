bef4c9effba0bd9bb92aabcb57054e78bf362907
Quan Yuan <quabug@gmail.com>
2022-03-30 17:29:32 +0800

    upgrade version / simplify `ManagedBlobAssetReference`

--- Packages/com.quabug.blob/Runtime/Blob/ManagedBlobAssetReference.cs
+++ Packages/com.quabug.blob/Runtime/Blob/ManagedBlobAssetReference.cs
@@ -1,19 +1,28 @@
-﻿namespace Blob
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Blob
 {
     public unsafe class ManagedBlobAssetReference<T> where T : unmanaged
     {
         private readonly byte[] _blob;
-        private readonly T* _value;
 
-        public ref T Value => ref *_value;
-        public T* UnsafePtr => _value;
+        public ref T Value => ref *UnsafePtr;
+        public T* UnsafePtr
+        {
+            get
+            {
+                fixed (void* ptr = &_blob[0]) return (T*)ptr;
+            }
+        }
+
         public int Length => _blob.Length;
         public byte[] Blob => _blob;
 
-        internal ManagedBlobAssetReference(byte[] blob)
+        internal ManagedBlobAssetReference([NotNull] byte[] blob)
         {
+            if (blob.Length == 0) throw new ArgumentException("BLOB cannot be empty");
             _blob = blob;
-            fixed (void* ptr = &blob[0]) _value = (T*)ptr;
         }
     }
 }
