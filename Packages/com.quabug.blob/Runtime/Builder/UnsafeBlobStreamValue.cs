8a43c04fac56985ebe3ed93164ec5d851e58ff89
quabug <quabug@gmail.com>
2022-04-09 20:10:44 +0800

    `UnsafeBlobStreamValue`

---
+++ Packages/com.quabug.blob/Runtime/Builder/UnsafeBlobStreamValue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Blob
+{
+    public unsafe ref struct UnsafeBlobStreamValue<T> where T : unmanaged
+    {
+        private readonly IBlobStream _stream;
+        private readonly int _position;
+
+        public UnsafeBlobStreamValue([NotNull] IBlobStream stream) : this(stream, stream.DataPosition) {}
+        public UnsafeBlobStreamValue([NotNull] IBlobStream stream, int position)
+        {
+            _stream = stream;
+            _position = position;
+            if (stream.Length <= position) throw new ArgumentException("invalid position");
+        }
+
+        public ref T Value
+        {
+            get
+            {
+                fixed (void* ptr = &_stream.Buffer[_position])
+                {
+                    return ref *(T*)ptr;
+                }
+            }
+        }
+    }
+}
